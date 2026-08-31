37281cfd2ebc6eec80c033c9a4f09d4e47b0be6a
Ujimych <ujimych@gmail.com>
2026-08-31 20:38:57 +0300

    Create protocol_irene_remote.py

---
+++ plugins/protocol_irene_remote.py
@@ -0,0 +1,11 @@
+# ==========================================================
+# Irene Remote — дополнительные WebSocket-протоколы
+# ==========================================================
+
+PROTOCOL_OUT_VOLUME = 'out.volume'
+MT_OUT_VOLUME_COMMAND = f'{PROTOCOL_OUT_VOLUME}/command'
+MT_OUT_VOLUME_RESULT = f'{PROTOCOL_OUT_VOLUME}/result'
+
+PROTOCOL_OUT_LISTEN = 'out.listen'
+MT_OUT_LISTEN_COMMAND = f'{PROTOCOL_OUT_LISTEN}/command'
+MT_OUT_LISTEN_RESULT = f'{PROTOCOL_OUT_LISTEN}/result'
